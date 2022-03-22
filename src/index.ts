64735b84070b06ebdaa9ff16c39f83695b9f393b
supendi <irpan.supendi@gmail.com>
2022-03-22 09:41:04 +0700

    rename interface

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-export interface Languages {
+export interface LanguageCodes {
     id?: string
     en?: string
 }
 
-export type Dictionary<T> = { [x in keyof T]: Languages }
+export type Dictionary<T> = { [x in keyof T]: LanguageCodes }
 
 export const diksio = {
-    get: <TDictionary>(dictionary: TDictionary, key: keyof TDictionary, lang: keyof Languages): string | undefined => {
+    get: <TDictionary>(dictionary: TDictionary, key: keyof TDictionary, lang: keyof LanguageCodes): string | undefined => {
         var value = dictionary[key]
         if (value) {
-            const languages = value as Languages
+            const languages = value as LanguageCodes
             if (languages) {
                 return languages[lang]
             }
