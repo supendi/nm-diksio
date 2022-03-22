99ef2fe02635312bea85e2aeee87358499623c18
supendi <irpan.supendi@gmail.com>
2022-03-22 12:27:58 +0700

    update dictionary argument type

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ export interface LanguageCodes {
 export type Dictionary<T> = { [x in keyof T]: LanguageCodes }
 
 export const diksio = {
-    get: <TDictionary>(dictionary: TDictionary, key: keyof TDictionary, lang: keyof LanguageCodes): string | undefined => {
+    get: <TDictionary>(dictionary: Dictionary<TDictionary>, key: keyof TDictionary, lang: keyof LanguageCodes): string | undefined => {
         var value = dictionary[key]
         if (value) {
-            const languages = value as LanguageCodes
+            const languages = value
             if (languages) {
                 return languages[lang]
             }
